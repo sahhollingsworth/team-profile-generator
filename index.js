fad8fa6e5b1e6790e538f1d062ab4d9490b3bd8b
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-10-17 22:20:26 -0700

    Feat:Added empty array for person objects from input.  Built functions for all user prompts and writing results to html file output.
    Details: Function for Manager, Function for Employee. Function for Engineer, which inherits from Employee. Function for Intern, which inherits from Employee. Function to add more employees or finish input. Function to write user response to html file output.

--- index.js
+++ index.js
@@ -5,43 +5,173 @@ const fs = require("fs");
 // Responsible for formatting HTML file output with user input values
 const generateHTMLTemplate = require("./lib/generateHTMLTemplate.js");
 
-// Calling the team object modules
+// Calling the team person object modules
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("/lib/Engineer");
 const Intern = require("./lib/Intern");
 
-inquirer
-    // questions for employee class
-    .prompt ([
+// Empty Array to store all team person objects created from user input
+var team = [];
+
+// Function to prompt Manager-specific questions
+function addManager() {
+    inquirer
+        .prompt ([
+            {
+                type: "input",
+                message: "What is the Manager's name?",
+                name: "name",
+            },
+            {
+                type: "input",
+                message: "What is the Manager's employee ID number?",
+                name: "employeeId",
+            },
+            {
+                type: "input",
+                message: "What is the Manager's ID email address?",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "What is the Manager's ID office number?",
+                name: "officeNum",
+            },
+        ])
+        .then(response => {
+            // create a Manager object with information from prompt
+            const employeeManager = new Manager(response.name, response.id, response.email, response.officeNum);
+            // Add this Manager person object to the team array
+            team.push(employeeManager);
+            // call function that prompts if user done or wants to add another employee
+            addAnother();
+        })
+}
+
+// Functionn to prompt for employee information, used for all non-Manager team additions
+function addEmployee () {
+    inquirer
+    .prompt([
         {
             type: "input",
-            message: "What is the Manager's name?",
+            message: "What is this team member's name?",
             name: "name",
         },
         {
             type: "input",
-            message: "What is the Manager's employee ID number?",
+            message: "What is this team member's employee ID number?",
             name: "employeeId",
         },
         {
             type: "input",
-            message: "What is the Manager's ID email address?",
+            message: "What is this team member's ID email address?",
             name: "email",
         },
         {
-            type: "input",
-            message: "What is the Manager's ID office number?",
-            name: "officeNum",
+            type: "list",
+            message: "What is this team member's role?",
+            choices: ["Engineer", "Intern"],
+            name: "role",
         },
+    ])
+    // Evaluate user input (response)
+    .then(response => {
+        // if employee is Engineer, then ask additionl Engineer-specific prompt(s)
+        if(response.role === "Engineer") { 
+            addEngineer(response.name, response.id, response.email);
+        }
+        // if employee is Inter, then ask additionl Intern-specific prompt(s)
+        else if (response.role === "Intern") {
+            addIntern(response.name, response.id, response.email);
+        }
+    })
+}
+
+// Function to prompt engineer-specific questions in addition to employee questions
+function addEngineer () {
+    inquirer
+    // User input prompts
+    .prompt([
+        {
+            type: "input",
+            message: "What is this team member's GitHub username?",
+            name: "githubUser",
+        },
+    ])
+    // Evaluate user input (response)
+    .then(response => {
+        // create a Engineer object with information from prompt
+        const employeeEngineer = new Engineer(response.name, response.id, response.email, response.githubUser);
+        // Add this Engineer person object to the team array
+        team.push(employeeEngineer);
+        // call function that prompts if user done or wants to add another employee
+        addAnother();
+    })
+}
+
+// Function to prompt intern-specific questions in addition to standard employee questions
+function addIntern () {
+    inquirer
+    // User input prompts
+    .prompt([
+        {
+            type: "input",
+            message: "What school does this team member attend?",
+            name: "school",
+        },
+    ])
+    // Evaluate user input (response)
+    .then(response => {
+        // create a Intern object with information from prompt
+        const employeeIntern = new Intern(response.name, response.id, response.email, response.school);
+        // Add this Intern person object to the team array
+        team.push(employeeIntern);
+        // call function that prompts if user done or wants to add another employee
+        addAnother();
+    })
+}
+
+
+// Function to either complete user prompts or restart add employee prompts to add another teammate to the team array
+function addAnother() {
+    inquirer
+    // User input prompts
+    .prompt([
         {
             type:"confirm",
             message:"Would you like to add another team member to the team?",
-            name: "addTeammate",
+            name: "addAnother",
         },
     ])
-    //
+    // Evaluate user input (response)
     .then (response => {
-        // if response to addTeammate is true, then ask questions related to that team member type
-        // if response to addTeammate is false, then write to thh html file using the template file
+        // If more employees to add, run addEmployee prompts
+        if(response.addAnother === true) {
+            addEmployee();
+        }
+        // if no more employees to add, call function to build index.html with completed team array
+        else {
+            buildPage("./dist/index.html", team);
+        }
     })
+}
+
+// Should this be in the addAnother() function? pro: consolidates code that only occurs as a result of this function. cons: overly dense. The action in the function is independent of addAnother and could theoretically be called else on the future
+//
+function buildPage(file, data) {
+    // Create a file called PLACHOLDERTBD.HTML, adding the input data transformed by SOMEFILETBD.js formatting.
+    fs.writeFile(/*PLACHOLDERTBDe.html*/, /*formatfunctioninSOMEFILETBD(data)*/, function (error) {
+        // If file created successfull, notify user of success in the command line interface. If error, notify user in the command line interface.
+        if(error) {
+            console.log("An unknown error occurred.")
+        }
+        else {
+            console.log("Your Team Profile page has been created successfully!")
+        }
+    })
+}
+
+
+// Start app logic, assumption is user is Manager
+addManager();
